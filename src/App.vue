93ac1834ca65a8b69131f71052c2b2dc502313b3
merel <hahahahmerel@gmail.com>
2026-07-28 13:49:18 +0200

    scrolling text!

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,10 @@
-<script setup></script>
+<script setup>
+import ScrollingText from './components/ScrollingText.vue'
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
-  <p>why not working</p>
+  <ScrollingText text="hello" color="white" />
+  <p>help</p>
 </template>
 
 <style scoped></style>
