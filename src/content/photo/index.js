a9d683bf1106ad9f4039b07bc99ff4a5f6b6bd1e
merel <hahahahmerel@gmail.com>
2026-09-24 19:46:32 +0200

    photos2

--- src/content/photo/index.js
+++ src/content/photo/index.js
@@ -6,12 +6,27 @@ import photo4 from '@/assets/photos/jacobindehout/Untitled-1_04.jpg'
 import photo5 from '@/assets/photos/jacobindehout/Untitled-1_05.jpg'
 //import photo6 from '@/assets/photos/jacobindehout/polaroid_small.png'
 
+import photo6 from '@/assets/photos/sketches_24092026/Untitled-5.jpg'
+import photo7 from '@/assets/photos/sketches_24092026/Untitled-4.jpg'
+import photo8 from '@/assets/photos/sketches_24092026/Untitled-2.jpg'
+import photo9 from '@/assets/photos/sketches_24092026/Untitled-3.jpg'
+import photo10 from '@/assets/photos/sketches_24092026/Untitled-1.jpg'
+import photo11 from '@/assets/photos/sketches_24092026/Untitled-6.jpg'
+
+
 
 export default [
+  {
+    photos: [ photo6, photo7, photo8, photo9, photo10, photo11 ],
+    date: '24.09.2026',
+    caption: 'zie, ik loop nu stage op echt de beste plek ooit! maar niet ech tbevordelijk voor de bochel. maar ik teken nog wel soms'
+  },
+
     {
         photos: [photo1, photo2, photo3, photo4, photo5],
         date: '31.07.2026',
         caption: 'jacob in de hout kiekjes. ga het missen ;-;'
-    },
+    }
+
 
 ]
